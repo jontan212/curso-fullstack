9ae90072caac7ca58fafc1c7887f0f0cc7670edc
Jonathan <jontan212yt@gmail.com>
2022-06-01 14:16:08 +0200

    layouts visto y terminado de java

---
+++ borrador/curso_java/pildoras_informaticas_java/PrimerosPasos/src/graficos/Layouts_I.java
@@ -0,0 +1,43 @@
+package graficos;
+
+import java.awt.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Layouts_I {
+	public static void main(String[] args) {
+		Marco_Layout marco = new Marco_Layout();
+		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		marco.setVisible(true);
+	}
+}
+
+class Marco_Layout extends JFrame {
+	public Marco_Layout() {
+		setTitle("Prueba Acciones");
+		setBounds(600, 350, 600, 300);
+		Panel_Layout lamina = new Panel_Layout();
+		Panel_Layout2 lamina2 = new Panel_Layout2();
+		add(lamina, BorderLayout.NORTH);
+		add(lamina2, BorderLayout.SOUTH);
+	}
+}
+
+class Panel_Layout extends JPanel {
+	public Panel_Layout() {
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+		add(new JButton("Amarillo"));
+		add(new JButton("Rojo"));
+	}
+}
+
+class Panel_Layout2 extends JPanel {
+	public Panel_Layout2() {
+		setLayout(new BorderLayout());
+		add(new JButton("Azul"), BorderLayout.WEST);
+		add(new JButton("Verde"), BorderLayout.EAST);
+		add(new JButton("Negro"), BorderLayout.CENTER);
+	}
+}
