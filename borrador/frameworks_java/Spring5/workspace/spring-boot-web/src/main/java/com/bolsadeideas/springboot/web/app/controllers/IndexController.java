f0509ca082844ec45c5fd7e45258f60c17ec0d93
Jonathan <jontan212yt@gamil.com>
2022-06-29 23:12:41 +0200

    hasta el video 12 de spring boot

--- borrador/frameworks_java/Spring5/workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ borrador/frameworks_java/Spring5/workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,12 +1,17 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
-	@GetMapping({"/index","/","/home"})
-	public String index() {
+	@GetMapping({ "/index", "/", "/home" })
+	public String index(Model mv) {
+		mv.addAttribute("titulo", "hola Spring Framework con ModelAndView!");
 		return "index";
 	}
 }
